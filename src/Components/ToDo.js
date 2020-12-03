61dade322a128b1050a7ecf28f41ededb9d9387a
shedupte <shedupte@gmail.com>
2020-12-02 20:56:31 -0500

    completed button function added

--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -4,10 +4,22 @@ const ToDo= ({text,todo,todos,setToDos}) => {
     const deleteHandler = () =>{
         setToDos(todos.filter((el)=> el.id !== todo.id));
     };
+    const completeHandler = () => {
+        setToDos(todos.map((item) => {
+            if(item.id===todo.id){
+                return{
+                    ...item, completed: !item.completed
+                }
+            }
+            return item;
+        }))
+    }
     return(
         <div className="todo">
-            <li className="todo-item"> {text}</li>
-            <button className="complete-btn">
+            <li className={`todo-item ${todo.completed? "completed" : ""}`}>
+                {text}
+            </li>
+            <button onClick={completeHandler} className="complete-btn">
                 <i className="fas fa-check"></i>
             </button>
             <button onClick={deleteHandler} className="trash-btn">
